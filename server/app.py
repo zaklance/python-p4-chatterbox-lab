b2f17b45c0e21e4e37c13bd89e3e23703c014f7e
zaklance <wosewick@gmail.com>
2024-06-26 14:56:33 -0400

    done

--- server/app.py
+++ server/app.py
@@ -14,13 +14,37 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        msgs = Message.query.order_by(Message.created_at).all()
+        return [msg.to_dict() for msg in msgs], 200
+    elif request.method == 'POST':
+        data = request.get_json()
+        new_msg = Message(
+            username=data.get('username'),
+            body=data.get('body')
+        )
+        db.session.add(new_msg)
+        db.session.commit()
+        return new_msg.to_dict(), 201
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=['PATCH'])
 def messages_by_id(id):
-    return ''
+    if request.method == 'PATCH':
+        msg = Message.query.filter(Message.id == id).first()
+
+        if not msg:
+            return {'error': 'message not found'}
+        
+        data = request.get_json()
+
+        msg.body = data.get('body')
+
+        db.session.add(msg)
+        db.session.commit()
+
+        return msg.to_dict(), 200
 
 if __name__ == '__main__':
     app.run(port=5555)
